5cb12d69ebf029e932280aedd9318e8d1a004096
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-12-09 22:59:23 -0300

    feat: utiliza recurso de paginação e ordenação de médicos

--- api/api/src/main/java/med/voll/api/controllers/MedicoController.java
+++ api/api/src/main/java/med/voll/api/controllers/MedicoController.java
@@ -3,10 +3,17 @@ package med.voll.api.controllers;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,5 +26,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
     }
 }
