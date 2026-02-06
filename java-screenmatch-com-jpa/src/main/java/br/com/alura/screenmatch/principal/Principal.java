5e3acecaeed4446cb59c64cc924f1a40fb845163
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-06 10:24:25 -0300

    feat: utiliza derived querys com ordenação e filtros de limitação e de trazer dados de forma decrescente

--- java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -44,6 +44,7 @@ public class Principal {
                     3 - Listar séries buscadas
                     4 - Buscar série por título
                     5 - Buscar séries por Ator
+                    6 - Top 5 Séries
                     0 - Sair                                 
                     """;
 
@@ -70,6 +71,9 @@ public class Principal {
                     break;
                 case 5:
                     buscarSeriesPorAtor();
+                    break;
+                case 6:
+                    buscarTop5Series();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -155,5 +159,9 @@ public class Principal {
         System.out.println("Série em que " + nomeAtor + " trabalhou");
         seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + " " + s.getAvaliacao()));
 
+    }
+    private void buscarTop5Series() {
+        List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        serieTop.forEach(s -> System.out.println(s.getTitulo() + " " + s.getAvaliacao()));
     }
 }
